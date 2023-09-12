854bc57c998cdad9b75f4b266a10c267dc70db35
blacklite <blacklite21`@gmail.com>
2023-09-12 20:51:28 +0700

    сделал шахматную раскраску. надо теперь красиво красить каждую клетку

--- tests/test.ts
+++ tests/test.ts
@@ -2,18 +2,42 @@ $(() => {
 	const canvas: JQuery<HTMLCanvasElement> = $('canvas');
 	const ctx: CanvasRenderingContext2D = canvas[0].getContext('2d');
 
-	let color = 'rgb(255,0,0)';
-	let offset = 10
+	const SIZE_CELL = 25;
 
-	for (let x = 0; x < canvas[0].width; x += 10)
+	let R		: number = 255;
+	let G		: number = 0;
+	let B		: number = 0;
+
+	//0 128 255
+
+	let count	: number = 0;
+	let flag	: boolean = false;
+
+
+	const AMOUNT_IN_COL: number = canvas[0].height / (SIZE_CELL * 2);
+	const AMOUNT_IN_ROW: number = canvas[0].width / (SIZE_CELL * 2);
+	const AMOUNT_IN_DIAGONAL: number = (canvas[0].width / (SIZE_CELL * 2)) * 2;
+
+	console.log('AMOUNT_IN_COL', AMOUNT_IN_COL);
+	console.log('AMOUNT_IN_ROW', AMOUNT_IN_ROW);
+	console.log('AMOUNT_IN_DIAGONAL', AMOUNT_IN_DIAGONAL);
+
+	for (let x = 0; x < canvas[0].width; x += SIZE_CELL)
 	{
+		// R++;
 
-		for (let y = 0; y < canvas[0].height; y += offset)
+
+		G += 5;
+		B += 5;
+
+		for (let y = flag ? SIZE_CELL : 0; y < canvas[0].height; y += SIZE_CELL*2)
 		{
-			ctx.fillStyle = color;
-			ctx.fillRect(x, y, 10, 10);
+			ctx.fillStyle = `rgb(${R},${G},${B})`;
+			ctx.fillRect(x, y, SIZE_CELL, SIZE_CELL);
 		}
-		offset++;
+		count++;
+		flag = !flag;
 	}
 
+	console.log(G);
 })
