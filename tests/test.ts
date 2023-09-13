e5639762337d9b1d905b179dfbdf724e85ae0009
blacklite <blacklite21`@gmail.com>
2023-09-14 00:48:16 +0700

    Решил проблемы с CORS просто загружая картику через input file.

--- tests/test.ts
+++ tests/test.ts
@@ -1,26 +1,40 @@
 $(() => {
 	const canvas: JQuery<HTMLCanvasElement> = $('canvas');
-	const ctx: CanvasRenderingContext2D = canvas[0].getContext('2d');
+	const ctx: CanvasRenderingContext2D = canvas[0].getContext('2d', { willReadFrequently: true }) as CanvasRenderingContext2D;
 
-	const image = new Image();
-	image.src = 'img25.jpg';
-	image.crossOrigin = "Anonymous";
 
-	image.onload = () => {
-		ctx.drawImage(image, 0,0, canvas.width(), canvas.height());
 
-		const imageData: ImageData = ctx.getImageData(0, 0, canvas.width(), canvas.height());
+	const fileData: JQuery<HTMLInputElement> = $('input');
 
-		for (let i = 0; i < imageData.data.length; i += 4)
+	fileData.on('change', () =>
+	{
+		const file		: File = fileData[0].files[0];
+		const reader	: FileReader = new FileReader();
+
+		reader.onload = () =>
 		{
-			const greyVersion = imageData.data[i] + imageData.data[i+1] + imageData.data[i+2]
-			imageData.data[i] = greyVersion;
-			imageData.data[i+1] = greyVersion;
-			imageData.data[i+2] = greyVersion;
-		}
+			const image = new Image();
+			image.src = reader.result.toString();
 
-		ctx.putImageData(imageData, 0, 0);
-		//
-		// console.log(imageData);
-	}
+			image.onload = () =>
+			{
+				ctx.drawImage(image, 0,0, canvas.width(), canvas.height());
+
+				const imageData: ImageData = ctx.getImageData(0, 0, canvas.width(), canvas.height());
+
+				for (let i = 0; i < imageData.data.length; i += 4)
+				{
+					const greyVersion = imageData.data[i] + imageData.data[i+1] + imageData.data[i+2];
+					imageData.data[i] = greyVersion;
+					imageData.data[i+1] = greyVersion + 10;
+					imageData.data[i+2] = greyVersion - 100;
+				}
+
+				ctx.putImageData(imageData, 0, 0);
+			}
+
+		};
+
+		if (file) reader.readAsDataURL(file);
+	});
 });
